7a4228392e85de7776be0ac984757e794809477e
bestkosov <kosov@karazin.ua>
2021-03-20 18:21:16 +0200

    matrix homework

--- src/topic1/lesson3/matrix.py
+++ src/topic1/lesson3/matrix.py
@@ -7,19 +7,32 @@ if __name__ == '__main__':
 
     # TODO print ALL elements of matrix
     for i in range(len(M1)):
-        for j in range(len(M1)):
+        for j in range(len(M1) + 1):
             print(M1[i][j])
+
+    # TODO print ALL elements of matrix
+    for i in M1:
+        for j in i:
+            print(j)
 
     M2 = [[1, 2, 3, 4],
           [5, -6, 7, 8],
           [5, 6, 5, 8],
           [1, 6, 7, 11]]
     # TODO print diagonal - 1,-6,5,11
-    print(M2)
+    s = 0
+    while s < len(M2):
+        for i in range(len(M2)):
+            print(M2[i][s])
+            s += 1
 
     M3 = [[1, 2, 3, 4],
           [5, 2, 7, 8],
           [5, 6, 3, 8],
           [1, 6, 7, 4]]
     # TODO print diagonal in oposite order  4, 3, 2, 1
-    print(M3)
+    s = len(M3) - 1
+    while s >= 0:
+        for i in reversed(range(len(M3))):
+            print(M3[i][s])
+            s -= 1
