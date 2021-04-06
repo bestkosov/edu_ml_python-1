cbbdd6044609b56639f5c537be455c2a025f8b50
bestkosov <kosov@karazin.ua>
2021-04-06 21:22:27 +0300

    homework_word_to_vector_matrix

--- src/topic2/word_to_vector.py
+++ src/topic2/word_to_vector.py
@@ -15,17 +15,30 @@ Word Embeddings (CBO)
 """
 
 # Do not change the sentences
-sentence1 = "Socrates likes to watch movies. Mary likes movies too."
-sentence2 = "Rene Descartes likes to watch movies. Mary likes movies too."
-sentence3 = "Kant also likes to watch football games"
+inFile = open('input.txt', 'r')
+inFile = inFile.readlines()
+matrix = []
 
+def form_of_list(list_of_sentences):
+    """
+    Form list_of_sentences
+    """
+    mS = []
+    for i in inFile:
+        mS.append(i.replace('.', ' ').split())
+    return mS
 
 def collect_all_unique_words(list_of_sentences):
     """
-    1. all unique words have to be collected to the list
+    All unique words have to be collected to the list
     """
-    # TODO 1 use HOF 'map, filter, etc'
-    return ['a', 'b', 'c']
+    mS = form_of_list(inFile)
+    sS = []
+    for i in mS:
+        for j in i:
+            sS.append(j)
+    mS = list(set(sS))
+    return mS
 
 
 def to_lowercase_and_remove_stop_words(list_of_sentences):
@@ -35,21 +48,32 @@ def to_lowercase_and_remove_stop_words(list_of_sentences):
 
     """
     # TODO 2
-    return None
+    mS = collect_all_unique_words(inFile)
+    mS = list(map(str.lower, mS))
+    mS = list(filter(lambda x: len(x) > 2 and x != "also", mS))
+    return mS
 
 
-def create_matrix(list_of_sentences, count_of_columns):
-    """
-    type of the matrix will be 'int'
-    Title of the matrix == our unique words
-
-    TITLE---------------------'movies'--'watch'--'football'--'...'
-    3 line of the matrix 'int''   1  '--'  1  '--'   0   '--'...'
-                              '   1  '--'  1  '--'   0   '--'...'
-                              '   0  '--'  1  '--'   1   '--'...'
-    """
+def create_matrix(list_of_sentences):
     # TODO 3 collect all unique words from the all sentences
-    return None
+    mS = to_lowercase_and_remove_stop_words(inFile)
+    all_sent = form_of_list(inFile)
+    new_sent = []
+    for i in all_sent:
+        new_sent.append(list(map(str.lower, i)))
+    for i in range(len(all_sent)):
+        matrix.append([])
+    for i in mS:
+        for j in range(len(all_sent)):
+            if i in new_sent[j]:
+                matrix[j].append(1)
+            else:
+                matrix[j].append(0)
+    matrix.insert(0, mS)
+    return matrix
+
+print(create_matrix(inFile))
+inFile.close()
 
 
 if __name__ == '__main__':
